5bfa55020ffbf50d8ecf2940683a0ec413c81551
Tobias Brunsch <tobias.brunsch@aol.de>
2024-03-23 07:32:58 +0100

    Added sample TextContentStreamSample

---
+++ maXMLian-samples/src/dd/kms/maxmlian/samples/TextContentStreamSample.java
@@ -0,0 +1,36 @@
+package dd.kms.maxmlian.samples;
+
+import dd.kms.maxmlian.api.*;
+
+import java.io.ByteArrayInputStream;
+
+public class TextContentStreamSample
+{
+	private static final String	XML	= "<?xml version=\"1.0\" encoding=\"utf-8\" ?>"
+		+ "<sample>"
+		+	"part1"
+		+ 	"<element>"
+		+		"part2"
+		+	"</element>"
+		+	"part3a\n"
+		+	"part3b\n"
+		+	"part3c"
+		+ 	"<element>"
+		+		"part4"
+		+	"</element>"
+		+	"part5"
+		+ "</sample>";
+
+	public static void main(String[] args) throws XmlException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+		Document document = documentBuilder.parse(new ByteArrayInputStream(XML.getBytes()));
+		Element sample = document.getDocumentElement();
+		StringStream textContentStream = sample.getTextContentStream();
+		int i = 1;
+		String textContentPart;
+		while ((textContentPart = textContentStream.next()) != null) {
+			System.out.println("Junk " + i++ + ": " + textContentPart);
+		}
+	}
+}
